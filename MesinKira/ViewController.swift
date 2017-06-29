f27d58cb63181008922720238e9796e7ed11a76e
AFJ <afairiz@gmail.com>
2017-06-29 23:53:22 +0800

    Initial commit. Calculator keypad drawn.

---
+++ MesinKira/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MesinKira
+//
+//  Created by Ahmad Fairiz on 29/06/2017.
+//  Copyright © 2017 Recite Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+  @IBAction func touchDigit(_ sender: UIButton) {
+  }
+
+
+}
+
